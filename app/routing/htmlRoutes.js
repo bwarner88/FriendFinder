74e2be33cd78b4bf7a6b9df55fb76a940a9b9f79
unknown <brian_warner25@yahoo.com>
2018-07-21 18:54:33 -0700

    es6

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
-module.exports = function(app) {
-  app.get('/', function(req, res) {
+module.exports = (app) => {
+  app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/home.html'));
   });
 
-  app.get('/survey', function(req, res) {
+  app.get('/survey', (req, res) => {
       res.sendFile(path.join(__dirname, '../public/survey.html'));
   });
 };
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const body = require("body-parser");
 const path = require("path");
+
+port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
