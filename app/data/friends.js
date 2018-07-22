551e2faef361bf965f67c30d5a40163b1fe372c2
unknown <brian_warner25@yahoo.com>
2018-07-21 18:42:29 -0700

    pushed to github

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+
+const friends = [
+{
+    "name":"Zuul",
+    "photo":"https://gifimage.net/wp-content/uploads/2018/06/zuul-ghostbusters-gif-5.gif",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+"name": "Regan",
+"photo": "https://media.giphy.com/media/l2YWoWyZXRBxcvEJi/giphy.gif",
+"scores":[
+    4,
+    2,
+    1,
+    1,
+    5,
+    5,
+    1,
+    2,
+    1,
+    3,
+]
+  },
+  {
+    "name": "Ricky",
+    "photo": "../../images/bubbles.jpg",
+    "scores":[
+        2,
+        3,
+        5,
+        1,
+        5,
+        5,
+        4,
+        2,
+        1,
+        3,
+    ]
+      }
+]
+  
+module.exports = "friends";
