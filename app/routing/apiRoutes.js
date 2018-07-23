67ac9c9df318686bb5ac628a152be62499610595
unknown <brian_warner25@yahoo.com>
2018-07-22 20:38:55 -0700

    hopefully got it working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = require('../data/friends.js');
+var friends = require('../data/friends');
 
 
 module.exports = function(app) {
@@ -9,37 +9,71 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function(req, res) {
-        var difference = 40;
-        var matchName = '';
-        var matchPhoto = '';
+
+        var userMatch = {
+            name: "",
+            photo: "",
+            difference: 40
+        };
+
+        var userData = req.body;
+        var userScore = userData.scores;
+
+        var totalDifference;
+
+        for(var i = 0; i < friends.length; i++){
+            var matchedFriend = friends[i];
+            difference = 0;
+            console.log(matchedFriend.name);
+
+            for(var j = 0; j < matchedFriend.scores.length; j++) {
+                var matchedFriendScore = matchedFriend.scores[j];
+                var currentUserScore = userScore[j];
+
+                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(matchedFriendScore));
+            }
+
+                if(TotalDifference <= userMatch.difference) {
+                    userMatch.name = matchedFriend.name;
+                    userMatch.photo = matchedFriend.photo;
+                    userMatch.friendDifference = totalDifference
+                }
+            
+        }
+
+        friends.push(userData);
+        res.json(userMatch)
+    //     var difference = 40;
+    //     var matchName = '';
+    //     var matchPhoto = '';
 
        
-        friends.forEach(function(friend) {
-            var matchedScoresArray = [];
-            var totalDifference = 40;
+    //     friends.forEach(function(friend) {
+    //         var matchedScoresArray = [];
+    //         var totalDifference = 40;
 
-            function add(total, num) {
-                return total + num;
-            }
-            for (var i = 0; i < friends.scores.length; i++) {
-                matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
+    //         function add(total, num) {
+    //             return total + num;
+    //         }
+    //         for (var i = 0; i < friends.scores.length; i++) {
+    //             matchedScoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
 
-            }
+    //         }
 
-            totalDifference = matchedScoresArray.reduce(add, 0);
+    //         totalDifference = matchedScoresArray.reduce(add, 0);
 
-            if (totalDifference < difference) {
-                difference = totalDifference;
-                matchName = friend.name;
-                matchPhoto = friend.photo;
-            }
-        });
+    //         if (totalDifference < difference) {
+    //             difference = totalDifference;
+    //             matchName = friend.name;
+    //             matchPhoto = friend.photo;
+    //         }
+    //     });
 
-        res.json({
-            name: matchName,
-            photo: matchPhoto
-        });
+    //     res.json({
+    //         name: matchName,
+    //         photo: matchPhoto
+    //     });
 
-        friends.push(req.body);
+    //     friends.push(req.body);
     });
 }
--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
+
+var app  = express();
 
 PORT = process.env.PORT || 3000
-
-app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
